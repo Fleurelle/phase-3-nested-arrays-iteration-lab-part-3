82cf57976885bab984aa2cdda8b52fb25592b1ec
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2021-09-04 13:14:50 -0400

    Done

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,23 @@
+require "pry"
+
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  outer_array_index = 0
+
+  while outer_array_index < src.length do 
+    inner_array_index = 0
+
+    while inner_array_index < src[outer_array_index].length do
+      # Thought process: If the element is not equal to a string, delete it. 
+      if src[outer_array_index][inner_array_index].class != String
+        src[outer_array_index].delete(src[outer_array_index][inner_array_index])
+        # Had to include this line because integer elements that are right next to one another are disregarded due to their indices. QUick solution! 
+        inner_array_index -= 1
+      end
+      inner_array_index += 1
+    end
+    outer_array_index += 1
+  end
+  src.flatten().join(" ")
 end
